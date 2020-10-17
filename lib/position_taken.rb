6d87466ba5e10dd8e90bce0f3df54a88b7e303d4
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 23:12:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 
 
- def position_taken?(board(array))
-   if (board(array) == "" || board(array) == " ")
+ def position_taken?(board,array)
+   if (board,array) == "" || (board,array) == " ")
      puts false
  else 
    true
