daa7d992f034f63708adb07f2bd0f4423abed769
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 23:06:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,20 @@
 # code your #position_taken? method here!
+
+def position_taken?(board,index)
+ if (board, index) == " " 
+ return false
+ if (board, index) == ""
+ return false
+ if (board, index) ==  nil  
+ return false
+ else (board, index) 
+ return true
+ end
+end
+
+ def position_taken?(location)
+    !(position(location).nil? || position(location) == " ")
+  end
+def location
+  location = board[array]
+end
