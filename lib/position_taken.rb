1f2c5ef6efc476d4058c0765b65b0e5a867a662c
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 23:08:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,19 +4,5 @@
  def position_taken?(board(array))
     !(board(array).nil? || board(array) == " ")
   end
-def location
-  location = board(array)
-end
 
-ef position_taken?(board,index)
- if (board, index) == " " 
- return false
- if (board, index) == ""
- return false
- if (board, index) ==  nil  
- return false
- else (board, index) 
- return true
- end
-end
-d
+
